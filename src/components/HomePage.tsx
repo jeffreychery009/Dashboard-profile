f83a0abf0610f01438f17079644f6db256cf42d9
Jeffrey Chery <jeffchery009@gmail.com>
2024-07-31 05:13:32 -0400

    Styling and Markup for Homepage

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,42 @@
+import profileImage from "../assets/ProfileImage.jpg";
+import Menu from "../assets/list.svg";
+
+const HomePage = () => {
+  const user = "Jeffrey";
+
+  return (
+    <>
+      <div className="custom-mobile:flex justify-between items-center mb-5">
+        <img src={Menu} alt="" />
+        <h1 className="h1-style text-center ">Dashboard.io</h1>
+        <img
+          className="w-[28px] h-[28px] rounded-full"
+          src={profileImage}
+          alt=""
+        />
+      </div>
+      <div className="flex justify-center mb-5">
+        <input
+          className="search-input" // Added text-gray-300 class for light-grey text color
+          type="text"
+          placeholder="Search"
+        />
+      </div>
+      <div className="mb-4">
+        <span className="text-custom-text">Hi {user},</span>
+        <h2 className="text-md font-semibold py-1">
+          Welcome to your{" "}
+          <span className="bg-blue-gradient bg-clip-text text-transparent">
+            Dashboard
+          </span>
+        </h2>
+      </div>
+      <div>
+        <h2 className="text-md font-semibold text-custom-text">Overview</h2>
+      </div>
+      <div className="flex justify-center items-center"></div>
+    </>
+  );
+};
+
+export default HomePage;
