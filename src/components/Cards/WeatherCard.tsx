79e72c282475ba0e7509622dc228ea27c88efb80
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-02 06:01:17 -0400

    Created Weather Component

---
+++ src/components/Cards/WeatherCard.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+import { WeatherPropsData, getWeatherData } from "../../services/api-weather";
+
+const WeatherCard = () => {
+  const [weather, setWeather] = useState<WeatherPropsData | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          getWeatherData(latitude, longitude)
+            .then((weatherData) => {
+              setWeather(weatherData);
+            })
+            .catch(() => {
+              setError("Failed to fetch Weather");
+            });
+        },
+        () => setError("Failed to get Location") // Remove the unused 'error' parameter
+      );
+    } else {
+      setError("Geolocation is not supported in your browser");
+    }
+  }, []);
+
+  if (error) return <div>{error}</div>;
+  return (
+    <>
+      <div className="card-style">
+        <div className="flex justify-between mb-3">
+          <h3 className="text-sm font-medium">Weather</h3>
+          <h4 className="text-xs">My Location</h4>
+        </div>
+        <div className="flex justify-between mb-3">
+          <p className="font-medium">{weather?.city}</p>
+          <p className="bg-blue-gradient bg-clip-text text-transparent text-md font-medium">
+            {weather?.temperature}°F
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <p>{weather?.condition}</p>
+          <div className="flex bg-blue-gradient bg-clip-text text-transparent">
+            <span className="mx-2 ">L: {weather?.tempLowF}°</span>{" "}
+            <span>H: {weather?.tempHighF}°</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WeatherCard;
