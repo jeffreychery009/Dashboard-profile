956e34c08e597c91d322a3c169b4d9252774d932
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-02 12:28:09 -0400

    Created News Card and functionality

--- src/components/Cards/NewsCard.tsx
+++ src/components/Cards/NewsCard.tsx
@@ -1,5 +1,36 @@
+import { NewsProps, getNews } from "../../services/api-news";
+import { useEffect, useState } from "react";
+
 const NewsCard = () => {
-  return <div>NewsCard</div>;
+  const [news, setNews] = useState<NewsProps | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    getNews()
+      .then((res) => {
+        setNews(res[1]);
+      })
+      .catch(() => {
+        setError("Failure to Load Article");
+      });
+  }, []);
+
+  if (error) return <div>{error}</div>;
+
+  return (
+    <>
+      <div className="card-style">
+        <h3 className="text-sm font-medium mb-2">News</h3>
+        <h4 className="font-medium mb-3">Breaking News: {news?.title}</h4>
+        <a
+          className="text-xs bg-blue-gradient bg-clip-text text-transparent"
+          href="#"
+        >
+          Click to read more
+        </a>
+      </div>
+    </>
+  );
 };
 
 export default NewsCard;
