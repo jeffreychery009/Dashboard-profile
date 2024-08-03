50a8157865ecf4f0eab67b9d5f76984c866de63a
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-02 22:43:28 -0400

    Created Settings Card

---
+++ src/components/Cards/SettingsCard.tsx
@@ -0,0 +1,16 @@
+const SettingsCard = () => {
+  return (
+    <>
+      <div className="card-style">
+        <h3 className="text-sm font-medium mb-4">Settings</h3>
+        <p>
+          <a className="bg-blue-gradient bg-clip-text text-transparent" href="">
+            Access your settings
+          </a>
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default SettingsCard;
