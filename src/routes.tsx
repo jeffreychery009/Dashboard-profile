d438bda4b64c11f568bf13290dc81f708d46d448
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-07 05:24:17 -0400

    Configured Routing within different components

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import Settings from "./Pages/Settings";
 
-createBrowserRouter([]);
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/settings", element: <Settings /> },
+]);
+
+export default router;
