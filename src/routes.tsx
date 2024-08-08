af7cceff78a06ce8a0a4d963ab25d1037c1a2b93
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-08 06:08:24 -0400

    Configured an additional route

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Settings from "./Pages/Settings";
+import ProfilePage from "./Pages/ProfilePage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/settings", element: <Settings /> },
+  { path: "/profile", element: <ProfilePage /> },
 ]);
 
 export default router;
