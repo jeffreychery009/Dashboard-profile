3391995432bf2df88132bc7b60123cced5f1875f
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-09 13:51:43 -0400

    Additional changes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Settings from "./Pages/Settings";
 import ProfilePage from "./Pages/ProfilePage";
+import NewsPage from "./Pages/NewsPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/settings", element: <Settings /> },
   { path: "/profile", element: <ProfilePage /> },
+  { path: "/news", element: <NewsPage /> },
 ]);
 
 export default router;
