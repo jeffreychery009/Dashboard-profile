43adeb2eda87d37c6ed82ceb2927f4338d398838
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-09 15:37:03 -0400

    Configured an additional route

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,12 +3,14 @@ import HomePage from "./components/HomePage";
 import Settings from "./Pages/Settings";
 import ProfilePage from "./Pages/ProfilePage";
 import NewsPage from "./Pages/NewsPage";
+import WeatherPage from "./Pages/WeatherPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/settings", element: <Settings /> },
   { path: "/profile", element: <ProfilePage /> },
   { path: "/news", element: <NewsPage /> },
+  { path: "/weather", element: <WeatherPage /> },
 ]);
 
 export default router;
