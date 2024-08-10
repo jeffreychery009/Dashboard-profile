fa88fc1c35bf2fc0626a3615ac1cf16f3d6ee8e8
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-10 12:43:10 -0400

    Configured route in todolist card

--- src/components/Cards/TodoListCard.tsx
+++ src/components/Cards/TodoListCard.tsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const TodoListCard = () => {
   return (
     <>
       <div className="card-style">
-        <h3 className="text-sm font-medium mb-4">To-do List</h3>
-        {<p>Add a Task</p>}
+        <Link to="/todolist">
+          <h3 className="text-sm font-medium mb-4">To-do List</h3>
+          <p>Add a Task</p>
+        </Link>
       </div>
     </>
   );
