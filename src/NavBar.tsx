0cb537c93d914f10dcb5bde14ec38e680ff9fd8f
Jeffrey Chery <jeffchery009@gmail.com>
2024-08-30 16:36:49 -0400

    Implemented and Configured Navbar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import MenuContext from "./Context/ContextProvider";
+import { MdOutlineMenu } from "react-icons/md";
 
 const NavBar = () => {
+  const { activeMenu, setActiveMenu } = React.useContext(MenuContext);
+
+  useEffect(() => {
+    if (activeMenu) {
+      document.body.classList.add("overflow-x-hidden");
+    } else {
+      document.body.classList.remove("overflow-x-hidden");
+    }
+  }, [activeMenu]);
+
   return (
     <>
-      <div className="flex w-full bg-slate-500">
-        <h1>Hello</h1>
+      <div className="">
+        <div
+          className={`flex items-center w-full transition-all ${
+            activeMenu ? "transform translate-x-72" : ""
+          }`}
+        >
+          <button className={`py-4 ${!activeMenu ? "ml-4" : ""}`}>
+            <MdOutlineMenu
+              onClick={() => setActiveMenu(!activeMenu)}
+              className="text-2xl md:text-3xl"
+            />
+          </button>
+          <h1 className="text-center flex-1 mr-9 text-2xl md:text-3xl md:flex-1">
+            Dashboard.io
+          </h1>
+        </div>
       </div>
     </>
   );
