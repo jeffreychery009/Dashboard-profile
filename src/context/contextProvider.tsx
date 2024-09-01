2f37e3124f56edc353a0956ba327425a23a26bde
Jeffrey Chery <jeffchery009@gmail.com>
2024-09-01 03:53:18 -0400

    Defined Context Provider to export to other components

---
+++ src/context/contextProvider.tsx
@@ -0,0 +1,38 @@
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  ReactNode,
+  useContext,
+  useState,
+} from "react";
+
+// Define the shape of Props
+
+interface ContextProps {
+  activeMenu: boolean;
+  setActiveMenu: Dispatch<SetStateAction<boolean>>;
+}
+
+// Export the Context
+
+export const StateContext = createContext<ContextProps>({
+  activeMenu: true,
+  setActiveMenu: () => {},
+});
+
+type ContextProviderProps = {
+  children?: ReactNode;
+};
+
+export const ContextProvider = ({ children }: ContextProviderProps) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+
+  return (
+    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
