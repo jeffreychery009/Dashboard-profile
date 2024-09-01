aadcb67b7172965b17c30d679d4f8b753502b33d
Jeffrey Chery <jeffchery009@gmail.com>
2024-09-01 04:08:34 -0400

    Implementation of Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { IoIosMenu } from "react-icons/io";
+import { useStateContext } from "../context/contextProvider";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const { activeMenu, setActiveMenu } = useStateContext();
+
+  return (
+    <>
+      <div className="">
+        <div className="flex justify-between">
+          <button onClick={() => setActiveMenu(!activeMenu)}>
+            <IoIosMenu className="text-2xl" />
+          </button>
+          <h1 className="mx-auto md:ml-auto">Dashboard.io</h1>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
