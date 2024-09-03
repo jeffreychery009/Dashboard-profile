abc83da869dce4b75b7bb9af314d78a03dc858d6
Jeffrey Chery <jeffchery009@gmail.com>
2024-09-03 16:04:15 -0400

    Configured Contact page. Added search to filter through contacts

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { contacts } from "../data/DummyData";
 
 const Contacts = () => {
-  return <div>Contacts</div>;
+  const [search, setSearch] = useState("");
+
+  return (
+    <>
+      <div>
+        <div className=" max-w-80 mb-10 ">
+          <div className="bg-white shadow-md p-5 m-5 rounded-lg ">
+            <h1 className="font-bold md:text-2xl">Contacts</h1>
+            <p className="text-gray-500">Welcome to your contacts</p>
+          </div>
+        </div>
+        <div className="mb-20">
+          <input
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            className=" ml-5 w-full max-w-sm px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            placeholder="Search contact"
+          />
+        </div>
+
+        <div className="ml-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {contacts
+            .filter((contact) => {
+              return search.toLowerCase() === ""
+                ? contact
+                : contact.first_name.toLowerCase().includes(search);
+            })
+            .map((contact) => (
+              <div
+                key={contact.id}
+                className="bg-white p-4 shadow-md rounded-md"
+              >
+                <h1 className="text-xl font-medium">
+                  {contact.first_name} {contact.last_name}
+                </h1>
+                <p className="text-gray-400">{contact.email}</p>
+                <p className="text-gray-400">{contact.phone_number}</p>
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Contacts;
