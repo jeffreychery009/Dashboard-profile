1630c00048cccebc811ce421f8b69294b017f5e8
Jeffrey Chery <jeffchery009@gmail.com>
2024-09-03 03:15:08 -0400

    Implemented Navbar Functions and design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IoIosMenu } from "react-icons/io";
 import { useStateContext } from "../context/contextProvider";
 
 const NavBar = () => {
   const { activeMenu, setActiveMenu } = useStateContext();
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 769) {
+        setActiveMenu(false);
+      } else {
+        setActiveMenu(true);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-      <div className="">
-        <div className="flex justify-between">
+      <div>
+        <div className="flex justify-between items-center">
           <button onClick={() => setActiveMenu(!activeMenu)}>
             <IoIosMenu className="text-2xl" />
           </button>
-          <h1 className="mx-auto md:ml-auto">Dashboard.io</h1>
+          <div className="flex-1 flex justify-center">
+            <h1
+              className={
+                !activeMenu
+                  ? "mr-10 text-3xl font-medium"
+                  : "text-3xl font-medium lg:mr-52"
+              }
+            >
+              Dashboard.io
+            </h1>
+          </div>
         </div>
       </div>
     </>
