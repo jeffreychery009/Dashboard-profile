2799363e770970e1abaef7d27575f72762786927
Jeffrey Chery <jeffchery009@gmail.com>
2024-09-05 16:50:14 -0400

    Added darkMode to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class", // Adding dark mode to Project.
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 
   theme: {
