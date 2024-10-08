37e6e8a7baa0c34a5ee574ee9eceab89ea659221
Jeffrey Chery <jeffchery009@gmail.com>
2024-10-08 14:03:05 -0400

    Made changes. Added newly created components to the TodoPage

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useContext } from "react";
 import { TodoContext } from "../context/todoProvider";
+import TodoCard from "../components/TodoCard";
 import { Todo } from "../context/todoProvider";
+import Modal from "../components/Modal";
 
 const TodoPage: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -80,92 +82,39 @@ const TodoPage: React.FC = () => {
         </button>
       </div>
 
-      {/* Modal for Adding or Updating a Task */}
-      {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <div className="bg-white p-7 rounded-lg shadow-lg max-w-md w-full dark:bg-gray-900">
-            <h2 className="text-2xl font-medium mb-4 dark:text-gray-100">
-              {currentTodo.id !== null ? "Update Task" : "Add New Task"}
-            </h2>
-            <input
-              type="text"
-              name="title"
-              placeholder="Task Title"
-              value={currentTodo.title}
-              onChange={handleChange}
-              className="w-full mb-4 p-2 border rounded"
-            />
-            <textarea
-              name="description"
-              placeholder="Task Description"
-              value={currentTodo.description}
-              onChange={handleChange}
-              className="w-full mb-3 p-3 border rounded"
-            />
-            <div className="flex justify-end space-x-3">
-              <button
-                className="bg-gray-600 text-white px-4 py-2 rounded"
-                onClick={handleCloseModal}
-              >
-                Cancel
-              </button>
-              <button
-                className="bg-gradient-to-r from-[#0A42D4] to-[#20A2D9] text-white px-4 py-2 rounded"
-                onClick={handleSaveTask}
-              >
-                {currentTodo.id !== null ? "Update Task" : "Save Task"}
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+      <Modal
+        title={currentTodo.id !== null ? "Update Task" : "Add New Task"}
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onSave={handleSaveTask}
+      >
+        <input
+          type="text"
+          name="title"
+          placeholder="Task Title"
+          value={currentTodo.title}
+          onChange={handleChange}
+          className="w-full mb-4 p-2 border rounded"
+        />
+        <textarea
+          name="description"
+          placeholder="Task Description"
+          value={currentTodo.description}
+          onChange={handleChange}
+          className="w-full mb-3 p-3 border rounded"
+        />
+      </Modal>
 
       <div className="mt-10">
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 mb-6">
           {todos.map((todo) => (
-            <div
-              className={`bg-white p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 ml-4 dark:bg-gray-900 ${
-                todo.completed
-                  ? "bg-green-500 dark:bg-green-800 text-white"
-                  : ""
-              }`}
+            <TodoCard
               key={todo.id}
-            >
-              <h1
-                className={`text-xl font-medium mb-3 dark:text-gray-100 ${
-                  todo.completed ? "text-white" : ""
-                }`}
-              >
-                {todo.title}
-              </h1>
-              <p
-                className={`text-sm mb-6 dark:text-gray-300 ${
-                  todo.completed ? "text-white" : "text-gray-500"
-                }`}
-              >
-                {todo.description}
-              </p>
-              <div className="flex justify-between flex-wrap gap-2">
-                <button
-                  className="bg-purple-800 rounded-md text-white px-3 py-1 w-full sm:w-[100px] md:w-[120px] lg:w-auto font-light"
-                  onClick={() => deleteTodo(todo.id)}
-                >
-                  Delete
-                </button>
-                <button
-                  className="bg-green-700 rounded-md text-white px-3 py-1 w-full sm:w-[100px] md:w-[120px] lg:w-auto font-light"
-                  onClick={() => toggleTodo(todo.id)}
-                >
-                  Complete
-                </button>
-                <button
-                  className="bg-gradient-to-r from-[#0A42D4] to-[#20A2D9] rounded-md text-white px-3 py-1 w-full sm:w-[100px] md:w-[120px] lg:w-auto font-light"
-                  onClick={() => handleOpenModal(todo)}
-                >
-                  Update
-                </button>
-              </div>
-            </div>
+              todo={todo}
+              deleteTodo={deleteTodo}
+              toggleTodo={toggleTodo}
+              handleOpenModal={handleOpenModal}
+            />
           ))}
         </div>
       </div>
